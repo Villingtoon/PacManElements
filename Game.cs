439fd8382c531d6a6487be1f8e2dbf143e8aa75a
Noob gamer <legofanslv@gmail.com>
2020-08-27 21:06:30 +0300

    ver 21:06

--- Game.cs
+++ Game.cs
@@ -13,11 +13,14 @@ namespace PacManElements
     public partial class Game : Form
     {
         private Level level = new Level();
+        private Hero hero = new Hero();
+        private Timer mainTimer = null;
 
         public Game()
         {
             InitializeComponent();
             InitializeGame();
+            InitializeMainTimer();
         }
 
         private void InitializeGame()
@@ -26,6 +29,53 @@ namespace PacManElements
             this.Size = new Size(500, 500);
             //adding level to the game
             this.Controls.Add(level);
+            //adding hero to the game
+            this.Controls.Add(hero);
+            hero.BringToFront();
+            //add key down event handler
+            this.KeyDown += Game_KeyDown;
+
+        }
+
+        private void InitializeMainTimer()
+        {
+            mainTimer = new Timer();
+            mainTimer.Tick += MainTimer_Tick;
+            mainTimer.Interval = 20;
+            mainTimer.Start();
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            hero.Left += hero.HorizontalVelocity;
+            hero.Top += hero.VerticalVelocity;
+        }
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    hero.HorizontalVelocity = 0;
+                    hero.VerticalVelocity = -hero.Step;
+                    break;
+                case Keys.S:
+                    hero.HorizontalVelocity = 0;
+                    hero.VerticalVelocity = hero.Step;
+                    break;
+                case Keys.A:
+                    hero.HorizontalVelocity = -hero.Step;
+                    hero.VerticalVelocity = 0;
+                    break;
+                case Keys.D:
+                    hero.HorizontalVelocity = hero.Step;
+                    hero.VerticalVelocity = 0;
+                    break;
+                case Keys.F:
+                    hero.HorizontalVelocity = 0;
+                    hero.VerticalVelocity = 0;
+                    break;
+            }
         }
     }
 }
