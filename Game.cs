c88e1b1567bc04ed7bd058cff9aa62a37dc9efd3
Noob gamer <legofanslv@gmail.com>
2020-08-27 19:56:09 +0300

    ver 19:56

---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PacManElements
+{
+    public partial class Game : Form
+    {
+        private Level level = new Level();
+
+        public Game()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            //adjust game form size
+            this.Size = new Size(500, 500);
+            //adding level to the game
+            this.Controls.Add(level);
+        }
+    }
+}
